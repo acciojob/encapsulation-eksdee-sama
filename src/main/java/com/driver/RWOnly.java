d06fad35f8e0bfbeb40afe5fc950c1e2e3c717d3
Eksdee Sama <dsampatalwar@gmail.com>
2023-03-31 18:25:23 +0530

    Assignment Done

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
-public class RWOnly {
-    
+class RWOnly {
+    private String name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNameWriteOnly(String name) {
+        this.name = name;
+    }
 }
